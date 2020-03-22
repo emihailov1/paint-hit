e2137d8fb513f3cc6e889f8f022ad75e5ab1b2c0
Edgar <emihailovyt@gmail.com>
2020-03-22 10:17:23 +0200

    Coloring Circle

---
+++ Paint Hit/Assets/Scripts/Color Scripts/ColorChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision target)
+    {
+        if(target.gameObject.tag == "red")
+        {
+
+        }else
+        {
+            base.gameObject.GetComponent<Collider>().enabled = true;
+            GameObject gameObject = Instantiate(Resources.Load("splash1")) as GameObject;
+            gameObject.transform.parent = target.gameObject.transform;
+            Destroy(gameObject, 0.1f);
+            target.gameObject.name = "color";
+            StartCoroutine(ChangeColor(target.gameObject));
+        }
+    }
+
+    IEnumerator ChangeColor(GameObject g)
+    {
+        yield return new WaitForSeconds(0.1f);
+        MeshRenderer mesh = g.gameObject.GetComponent<MeshRenderer>();
+        mesh.enabled = true;
+        mesh.material.color = BallHandler.oneColor;
+        Destroy(base.gameObject);
+    }
+}
